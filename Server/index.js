b7a158d895c7100e9aaa409dd356c2c6c714b532
jaypedhadiya07 <jaypedhadiya76gmail.com>
2025-06-27 23:33:52 +0530

    Building a Http Server and Handling Url

---
+++ Server/index.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+const myServer = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return false;
+
+  let date = new Date();
+
+  const log = `${date.toLocaleTimeString()} New Request received ${req.url}\n`;
+
+  const Myurl = url.parse(req.url, true);
+  console.log(Myurl);
+
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (Myurl.pathname) {
+      case "/":
+        res.end("Home Page");
+        break;
+      case "/about":
+        res.end(`hii, ${Myurl.query.username} and ID: ${Myurl.query.id}`);
+        break;
+      case "/search":
+        const search = Myurl.query.search_query;
+        res.end(`Here are your Results for ${search}`);
+        break;
+      default:
+        res.end("404 Not found");
+        break;
+    }
+  });
+});
+
+myServer.listen(8000, () => console.log("Server Started"));
