426fe2ce0b7b98036ad54dc3fcb34e2d97143030
jaypedhadiya07 <jaypedhadiya76gmail.com>
2025-06-28 19:07:50 +0530

    Create a Project using HTTP Methods GET POST PATCH DELETE

---
+++ Express-project_01/index.js
@@ -0,0 +1,99 @@
+// Import required modules
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+const fs = require("fs");
+
+const app = express();
+const port = 8000;
+
+// Middleware - plugin to parse URL-encoded form data
+app.use(express.urlencoded({ extended: false }));
+
+// ------------------------------------------
+// Basic HTML Route: Display user first names
+// ------------------------------------------
+app.get("/users", (req, res) => {
+  const html = `
+    <ul>
+        ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+    </ul>`;
+  return res.send(html);
+});
+
+// ---------------------
+// REST API Routes
+// ---------------------
+
+// GET: List all users as JSON
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+// POST: Create a new user
+app.post("/api/users", (req, res) => {
+  // Get new user data from form
+  const body = req.body;
+  console.log(body);
+
+  // Add new user to the array with a new ID
+  users.push({ ...body, id: users.length + 1 });
+
+  // Save updated users array to the JSON file
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (error, data) => {
+    return res.json({ status: "success", id: users.length });
+  });
+});
+
+/*  ------------------------------
+    Alternative: Individual routes
+    ------------------------------
+app.get("/api/users/:ID", (req, res) => {
+  const id = Number(req.params.ID);
+  const user = users.find((user) => user.id === id);
+  return res.json(user);
+});
+
+app.patch("/api/users/:ID", (req, res) => {
+  //Todo: Edit the user with id
+  return res.json({ status: "pending" });
+});
+
+app.delete("/api/users/:ID", (req, res) => {
+  //Todo: Delete the user with id
+  return res.json({ status: "pending" });
+});
+*/
+
+// -------------------------------
+// Chained routes with .route()
+// -------------------------------
+app
+  .route("/api/users/:ID")
+  // GET: Fetch user by ID
+  .get((req, res) => {
+    const id = Number(req.params.ID);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+
+  // PATCH: Edit user by ID
+  .patch((req, res) => {
+    //Todo: Edit the user with id
+    const id = Number(req.params.ID);
+    const user = users.find((user) => user.id === id);
+    Object.assign(user, req.body);
+    return res.json(user);
+  })
+
+  // DELETE: Remove user by ID
+  .delete((req, res) => {
+    const id = Number(req.params.ID);
+    const index = users.findIndex((user) => user.id === id);
+    users.splice(index, 1);
+    return res.json({ status: "success" });
+  });
+
+// ----------------------------
+// Start the server
+// ----------------------------
+app.listen(port, () => console.log("Server Started at port: " + port));
